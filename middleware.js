39bda2bc6728fc4a9c79f5e6e6531b2fb86bd55d
Vanshyadav493 <vanshyadav5134@gmail.com>
2026-01-29 18:23:38 +0530

    add project files

---
+++ middleware.js
@@ -0,0 +1,64 @@
+const Listing = require("./models/listing");
+const Review = require("./models/review");
+const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
+
+
+module.exports.isLoggedIn = (req, res, next) => {
+    if(!req.isAuthenticated()) {
+      //redirecturl
+      req.session.redirectUrl  = req.originalUrl;
+  req.flash("error", "You Must Be Logged In To Create New Listing");
+   return res.redirect("/login");
+ }
+ next();
+};
+
+module.exports.saveRedirectUrl = (req, res, next) => {
+  if (req.session.redirectUrl) {
+    res.locals.redirectUrl = req.session.redirectUrl;
+  }
+  next();
+};
+
+module.exports.isOwner = async (req, res, next) => {
+  let { id } = req.params;
+      let listing = await Listing.findById(id);
+    if(!listing.owner.equals(res.locals.currUser._id)) {
+      req.flash("error", "you dint have access to edit");
+       return res.redirect(`/listings/${id}`);
+    };
+    next();
+};
+
+module.exports.validateListing = (req, res, next) => {
+    const { error } = listingSchema.validate(req.body);
+    if (error) {
+        // Remove "listing." from error messages
+        const msg = error.details.map(el => el.message.replace(/listing\./g, '')).join(", ");
+        throw new ExpressError(404, msg);
+    } else {
+        next();
+    }
+};
+
+module.exports.validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
+    if (error) {
+        // Remove "listing." from error messages
+        const msg = error.details.map(el => el.message.replace(/listing\./g, '')).join(", ");
+        throw new ExpressError(404, msg);
+    } else {
+        next();
+    }
+};
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+  let {id, reviewId } = req.params;
+      let review = await Review.findById(reviewId);
+    if(!review.author.equals(res.locals.currUser._id)) {
+      req.flash("error", "you did not create this review");
+       return res.redirect(`/listings/${id}`);
+    };
+    next();
+};
